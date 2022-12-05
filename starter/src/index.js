bdde585e85f27998e9904a98b4e8e7929fda9327
duyviet2111 <vietpd@tksvietnam.com>
2022-12-06 00:08:20 +0700

    First commit

--- starter/src/index.js
+++ starter/src/index.js
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Search from "./components/Search";
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/search" element={<Search />} />
+    </Routes>
+    {/* <App /> */}
+  </BrowserRouter>,
   document.getElementById("root")
 );
